25d07b635bd7ce755a808ef04728bf8ae338f5f6
Trevor Woerner <twoerner@gmail.com>
2011-05-28 17:44:19 -0400

    GPIO Lab 1 cleanup.

--- GPIO_lab1/gpio_lab1.c
+++ GPIO_lab1/gpio_lab1.c
@@ -1,108 +1,25 @@
-/********************************************************************
- FileName:     	<Application Name Here>.c
- Dependencies:	See INCLUDES section
- Processor:		PIC16F690 Microcontrollers
-
- Compiler:		HI-TECH C PRO for the PIC10/12/16 MCU Family (Lite)
- Company:		Microchip Technology, Inc.
- Engineer:		<Your Name Here>
-
- Software License Agreement:
-
- The software supplied herewith by Microchip Technology Incorporated
- (the "Company") for its PIC® Microcontroller is intended and
- supplied to you, the Company's customer, for use solely and
- exclusively on Microchip PIC Microcontroller products. The
- software is owned by the Company and/or its supplier, and is
- protected under applicable copyright laws. All rights are reserved.
- Any use in violation of the foregoing restrictions may subject the
- user to criminal sanctions under applicable laws, as well as to
- civil liability for the breach of the terms and conditions of this
- license.
-
- THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
- WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
- TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
- IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
- CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-
-********************************************************************
- File Description:
-
- Change History:
-  Rev   Date         Description
-
-
-********************************************************************/
-#include	<pic.h>
-
+#include <htc.h>
 
  //----------------------CONFIGURE DEVICE--------------------
- 
-	__CONFIG(FOSC_INTRCIO &	WDTE_OFF & PWRTE_OFF & MCLRE_OFF &
-			CP_OFF & CPD_OFF & BOREN_OFF & IESO_OFF & FCMEN_OFF);
-
-//-----------------------DATA MEMORY------------------------
-
-
-
-
-
-//----------------------PROGRAM MEMORY----------------------
-
+ __CONFIG(FOSC_INTRCIO & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & 
+		 CP_OFF & CPD_OFF & BOREN_OFF & IESO_OFF & FCMEN_OFF);
 
 /** PRIVATE PROTOTYPES *********************************************/
-void Initialize(void);
-void Get_Inputs(void);
-void Decide(void);
-void Do_Outputs(void);
-void Timing(void);
+static void initialize (void);
+static void do_outputs (void);
 
-
-
-
-
-/**SUPPORT ROUTINES*************************************************/
-
-
-
-
-/**INTERRUPT CODE*************************************************/
-
-
-
-
-/**SOFTWARE CONTROL LOOP CODE*************************************/
-/*----------------------------------------------------------
-	Subroutine: main
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Main program function
------------------------------------------------------------*/
-void main(void)
+/** SOFTWARE CONTROL LOOP CODE *************************************/
+void
+main (void)
 {
-
-	//ADD MAIN CODE HERE
-	Initialize ();
-
-	while (1) {
-		Do_Outputs ();
-	}
-	
+	initialize ();
+	do_outputs ();
+	while (1);
 }
 
-/*----------------------------------------------------------
-	Subroutine: INITIALIZE
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Initializes all registers 
-				associated with the application by calling
-				initialization functions for each peripheral
-----------------------------------------------------------*/
-void Initialize(void)
+static void
+initialize (void)
 {
-	//ADD INTIALIZE CODE HERE
 	PORTC = 0b00000000;
 
 	ANS4 = 0;
@@ -121,40 +38,10 @@ void Initialize(void)
 	TRISC6 = 0;
 	TRISC7 = 0;
 }
-	
-/*----------------------------------------------------------
-	Subroutine: Get_Inputs
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Obtains all off-chip inputs
-----------------------------------------------------------*/
-void Get_Inputs(void)
-{
-	//ADD GET INPUTS CODE HERE
-}
 
-/*----------------------------------------------------------
-	Subroutine: Decide
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Function that calls all sub-routines for 
-				processing the input data
-----------------------------------------------------------*/
-void Decide(void)
+static void
+do_outputs (void)
 {
-	//ADD DECIDE CODE HERE
-}
-
-/*----------------------------------------------------------
-	Subroutine: Do_Outputs
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Ouputs to off-chip circuitry based off of
-				Decide function
-----------------------------------------------------------*/
-void Do_Outputs(void)
-{
-	//ADD DO OUTPUTS CODE HERE
 	RC0 = 1;
 	RC1 = 0;
 	RC2 = 1;
@@ -164,16 +51,3 @@ void Do_Outputs(void)
 	RC6 = 0;
 	RC7 = 1;
 }
-
-/*----------------------------------------------------------
-	Subroutine: Timing
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Controls the how fast code in the main() is executed 
-----------------------------------------------------------*/
-void Timing(void)
-
-{
-	//ADD TIMING CODE HERE
-}
-
